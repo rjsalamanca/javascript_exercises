7c75a1b33add29f6add104c36aef2ceff4ec5479
RJ Salamanca <salamancaronito@gmail.com>
2019-05-08 11:10:54 -0400

    testing event listener

--- day18/scripts/script.js
+++ day18/scripts/script.js
@@ -1,11 +1,33 @@
 "use strict";
 
 const square = document.getElementById('square');
+const echo = document.getElementById('echo');
+const seven = document.getElementById('numberSeven');
 
-function sayHello(){
-  alert('Hello World!');
+function sayHello(name){
+  console.log(`Hello ${name}!`);
+}
+
+function promptForName(){
+  const name = prompt('What is your name?');
+  return name;
+}
+
+function echoThisText(textToRepeat){
+  console.log(textToRepeat);
 }
 
 square.addEventListener('click',function(){
-  sayHello();
+  const name = promptForName();
+  sayHello(name);
+});
+
+echo.addEventListener('click',function(){
+  const textToEcho = echo.innerHTML;
+  echoThisText(textToEcho);
+});
+
+seven.addEventListener('click',function(){
+  const value = numberSeven.innerHTML;
+  echoThisText(value);
 });
