48dfd596edde9ed5ece840581946f2e74722d375
RJ Salamanca <salamancaronito@gmail.com>
2019-05-08 13:03:44 -0400

    added event handlers for numbers, clear, operators

--- day18/learn-calculator/js/script.js
+++ day18/learn-calculator/js/script.js
@@ -9,9 +9,25 @@ const input = document.getElementById('input'), // input/output button
 let resultDisplayed = false; // flag to keep an eye on what output is displayed
 
 // adding click handlers to number buttons
+function addInput(num){
+    input.innerHTML += num
+}
+number.forEach(num=>{
+    num.addEventListener('click',function(){
+        addInput(num.innerHTML)
+    });
+})
 
 // adding click handlers to the operation buttons
+operator.forEach(op=>{
+    op.addEventListener('click',function(){
+        addInput(op.innerHTML)
+    });
+})
 
 // on click of 'equal' button
 
 // clearing the input on press of clear
+clear.addEventListener('click',function(){
+    input.innerHTML = ''
+})
