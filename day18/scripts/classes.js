7ed54187a32f94babfe4c48a3ad5e5d2c4590ebf
RJ Salamanca <salamancaronito@gmail.com>
2019-05-08 11:25:50 -0400

    working with node lists

--- day18/scripts/classes.js
+++ day18/scripts/classes.js
@@ -1,1 +1,10 @@
 "use strict";
+
+const listItems = document.querySelectorAll('.list-item');
+
+
+listItems.forEach(function(listItem){
+    listItem.addEventListener('click',function(){
+        console.log(listItem.innerHTML);        
+    });
+})
