b6ad602a041a25229edda452e1a6b6f34418904e
RJ Salamanca <salamancaronito@gmail.com>
2019-05-09 13:11:47 -0400

    added the calculations to the result event handler

--- day18/learn-calculator/js/script.js
+++ day18/learn-calculator/js/script.js
@@ -12,11 +12,11 @@ let lastInput = '';
 
 // adding click handlers to number buttons
 function addInput(addInput){
-    //lastInput = input.innerHTML != '' ? input.innerHTML[input.innerHTML.length-1] : '';
+    lastInput = input.innerHTML == '' ?  '' : input.innerHTML[input.innerHTML.length-1];
 
     if( (lastInput.match(regex) != null) && (addInput.match(regex) != null) ){
         if(lastInput != addInput){
-            let changeOperator = input.innerHTML.split('')
+            let changeOperator = input.innerHTML.split('');
             changeOperator.splice(input.innerHTML.length-1,1,addInput)
             input.innerHTML = changeOperator.join('')
         }
@@ -31,42 +31,61 @@ function addInput(addInput){
 
 number.forEach(num=>{
     num.addEventListener('click',function(){
-        lastInput = input.innerHTML != '' ? input.innerHTML[input.innerHTML.length-1] : '';
-        console.log(lastInput)
-        if(resultDisplayed === false){
-            addInput(num.innerHTML)
-        } else if ((resultDisplayed === true) && (lastInput == '+' || lastInput == '-' || lastInput == '*' || lastInput == '/')){
-            console.log('NO')
-            resultDisplayed = false;
-        } else{
-            resultDisplayed = false;
-            input.innerHTML = "";
-            addInput(num.innerHTML)
-        }
+        addInput(num.innerHTML);
     });
 })
 
 // adding click handlers to the operation buttons
 operator.forEach(op=>{
     op.addEventListener('click',function(){
-        addInput(op.innerHTML)
+        addInput(op.innerHTML);
     });
 })
 
 // on click of 'equal' button
 result.addEventListener('click',function(){
-    let getNumbers = input.innerHTML.split(regex).map(curr=>parseInt(curr));
-    let getOperators = input.innerHTML.split(/[0-9]/g).filter(curr=>curr != '');
+    let getNumbers = input.innerHTML.split(regex).map(curr=>Number(curr));
+    let getOperators = input.innerHTML.replace(/[0-9]|\./g,'').split('');
 
-    // Look for multiplication
+    lastInput = input.innerHTML == '' ?  '' : input.innerHTML[input.innerHTML.length-1];
 
-    // Look for division
+    if(!lastInput.match(regex)){
+        // Look for multiplication
+        let multiply = getOperators.indexOf('*');
+        while(multiply != -1){
+            getNumbers.splice(multiply,2, getNumbers[multiply] * getNumbers[multiply +1]);
+            getOperators.splice(multiply, 1);
+            multiply = getOperators.indexOf('*');
+        }
 
-    // Look for addition & subtraction
-    resultDisplayed = true;
-    console.log(getNumbers)
-    console.log(getOperators)
-})
+        // Look for division
+        let divide = getOperators.indexOf('/');
+        while(divide != -1){
+            getNumbers.splice(divide,2, getNumbers[divide] / getNumbers[divide +1]);
+            getOperators.splice(divide, 1);
+            divide = getOperators.indexOf('/');
+        }
+
+        // Look for addition & subtraction
+        let adddition = getOperators.indexOf('+');
+        while(adddition != -1){
+            getNumbers.splice(adddition,2, getNumbers[adddition] + getNumbers[adddition +1]);
+            getOperators.splice(adddition, 1)
+            adddition = getOperators.indexOf('+')
+        }
+
+        let subtraction = getOperators.indexOf('-');
+        while(subtraction != -1){
+            getNumbers.splice(subtraction,2, getNumbers[subtraction] - getNumbers[subtraction +1]);
+            getOperators.splice(subtraction, 1);
+            subtraction = getOperators.indexOf('-');
+        }
+
+        console.log(getNumbers);
+        resultDisplayed = true;
+        input.innerHTML = getNumbers;
+    }
+});
 
 // clearing the input on press of clear
 clear.addEventListener('click',function(){
