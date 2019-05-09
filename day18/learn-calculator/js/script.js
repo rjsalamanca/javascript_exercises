a3a18b7b513046f4a7887f443bdf82b82b7781a3
RJ Salamanca <salamancaronito@gmail.com>
2019-05-09 09:39:10 -0400

    updated operators, removed unicode

--- day18/learn-calculator/js/script.js
+++ day18/learn-calculator/js/script.js
@@ -4,7 +4,8 @@ const input = document.getElementById('input'), // input/output button
     number = document.querySelectorAll('.numbers div'), // number buttons
     operator = document.querySelectorAll('.operators div'), // operator buttons
     result = document.getElementById('result'), // equal button
-    clear = document.getElementById('clear'); // clear button
+    clear = document.getElementById('clear'), // clear button
+    regex = /\+|\-|\*|\//g; // Regex operators
     
 let resultDisplayed = false; // flag to keep an eye on what output is displayed
 
@@ -12,14 +13,14 @@ let resultDisplayed = false; // flag to keep an eye on what output is displayed
 function addInput(addInput){
     const lastInput = input.innerHTML != '' ? input.innerHTML[input.innerHTML.length-1] : '';
 
-    if( (lastInput.match(/\+|\-|\×|\÷/g) != null) && (addInput.match(/\+|\-|\×|\÷/g) != null) ){
+    if( (lastInput.match(regex) != null) && (addInput.match(regex) != null) ){
         if(lastInput != addInput){
             let changeOperator = input.innerHTML.split('')
             changeOperator.splice(input.innerHTML.length-1,1,addInput)
             input.innerHTML = changeOperator.join('')
         }
     } else {
-        if((lastInput == '') && (addInput.match(/\+|\-|\×|\÷/g) != null)){
+        if((lastInput == '') && (addInput.match(regex) != null)){
             console.log('No can\'t start with operator!');
         } else {
             input.innerHTML += addInput; 
@@ -42,7 +43,7 @@ operator.forEach(op=>{
 
 // on click of 'equal' button
 result.addEventListener('click',function(){
-    let getNumbers = input.innerHTML.split(/\+|\-|\×|\÷/g).map(curr=>parseInt(curr));
+    let getNumbers = input.innerHTML.split(regex).map(curr=>parseInt(curr));
     let getOperators = input.innerHTML.split(/[0-9]/g).filter(curr=>curr != '');
 
     // Look for multiplication
